63d53f1e205baf884eab77bf0c60f0589b670ed2
augustomachuca1991 <agguz.1991@gmail.com>
2026-06-30 01:49:49 -0300

    feat: enable immediate service worker registration in SWUpdatePrompt component

--- src/components/ui/SWUpdatePrompt.jsx
+++ src/components/ui/SWUpdatePrompt.jsx
@@ -4,7 +4,7 @@ export default function SWUpdatePrompt() {
   const {
     needRefresh: [needRefresh, setNeedRefresh],
     updateServiceWorker,
-  } = useRegisterSW();
+  } = useRegisterSW({ immediate: true });
 
   if (!needRefresh) return null;
 
